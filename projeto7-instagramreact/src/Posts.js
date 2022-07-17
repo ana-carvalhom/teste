44557e5d7ba34da1a3cd5fa6cc17202178cef6b2
Ana Carvalho <anapaulacmoura@gmail.com>
2022-07-17 20:43:37 -0300

    adicionando map nos posts

--- projeto7-instagramreact/src/Posts.js
+++ projeto7-instagramreact/src/Posts.js
@@ -61,82 +61,42 @@ function Posts(){
         userTitle="barked" 
         urlPost="assets/img/adorable_animals.svg" 
         mainLike="adorable_animals" 
-        otherLikes="outras 99.159 pessoas<"
+        otherLikes="outras 99.159 pessoas"
         />
     ]
+
+    const newItems = [
+    {
+        urlUser: 'assets/img/meowed.svg', 
+        userTitle: 'meowed', 
+        urlPost:'assets/img/gato-telefone.svg', 
+        mainLike:'respondeai', 
+        otherLikes:'outras 101.523 pessoas'
+    },
+    {
+        urlUser:'assets/img/meowed.svg', 
+        userTitle: 'barked', 
+        urlPost:'assets/img/adorable_animals.svg', 
+        mainLike:'adorable_animals', 
+        otherLikes:'outras 99.159 pessoas'
+    }               
+        ]
+        
+        const arrayPosts = newItems.map((item) => {
+            return <PostUser 
+            urlUser={item.urlUser}
+            userTitle={item.userTitle} 
+            urlPost={item.urlPost}
+            mainLike={item.mainLike}
+            otherLikes={item.otherLikes}
+            />;
+        })
+
+
+
     return (
         <div class="posts">
-            <div class="post">
-              <div class="topo">
-                <div class="usuario">
-                  <img src="assets/img/meowed.svg" />
-                  meowed
-                </div>
-                <div class="acoes">
-                  <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-              </div>
-
-              <div class="conteudo">
-                <img src="assets/img/gato-telefone.svg" />
-              </div>
-
-              <div class="fundo">
-                <div class="acoes">
-                  <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                  </div>
-                  <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
-                  </div>
-                </div>
-
-                <div class="curtidas">
-                  <img src="assets/img/respondeai.svg" />
-                  <div class="texto">
-                    Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="post">
-              <div class="topo">
-                <div class="usuario">
-                  <img src="assets/img/barked.svg" />
-                  barked
-                </div>
-                <div class="acoes">
-                  <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-              </div>
-
-              <div class="conteudo">
-                <img src="assets/img/dog.svg" />
-              </div>
-
-              <div class="fundo">
-                <div class="acoes">
-                  <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                  </div>
-                  <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
-                  </div>
-                </div>
-
-                <div class="curtidas">
-                  <img src="assets/img/adorable_animals.svg" />
-                  <div class="texto">
-                    Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {arrayPosts}
           </div>
     );
 }
