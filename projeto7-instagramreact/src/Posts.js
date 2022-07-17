7a4470956f4546e69477f7dc20fd993803f94b49
Ana Carvalho <anapaulacmoura@gmail.com>
2022-07-17 20:35:35 -0300

    adicionando funcionalidade map

--- projeto7-instagramreact/src/Posts.js
+++ projeto7-instagramreact/src/Posts.js
@@ -1,4 +1,69 @@
+function PostUser(props){
+console.log(props);
+
+return (
+    <div class="post">
+              <div class="topo">
+                <div class="usuario">
+                  <img src={props.urlUser} />
+                  {props.userTitle}
+                </div>
+                <div class="acoes">
+                  <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+              </div>
+
+              <div class="conteudo">
+                <img src={props.urlPost} />
+              </div>
+
+              <div class="fundo">
+                <div class="acoes">
+                  <div>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="chatbubble-outline"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                  </div>
+                  <div>
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                  </div>
+                </div>
+
+                <div class="curtidas">
+                  <img src="assets/img/respondeai.svg" />
+                  <div class="texto">
+                    Curtido por <strong>{props.mainLike}</strong> e <strong>{props.otherLikes}</strong>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+)
+}
+
+   
+
+
+
+
 function Posts(){
+
+    const post = [
+        <PostUser 
+        urlUser="assets/img/meowed.svg" 
+        userTitle="meowed" 
+        urlPost="assets/img/gato-telefone.svg" 
+        mainLike="respondeai" 
+        otherLikes="outras 101.523 pessoas"
+        />,
+        <PostUser 
+        urlUser="assets/img/barked.svg" 
+        userTitle="barked" 
+        urlPost="assets/img/adorable_animals.svg" 
+        mainLike="adorable_animals" 
+        otherLikes="outras 99.159 pessoas<"
+        />
+    ]
     return (
         <div class="posts">
             <div class="post">
--- projeto7-instagramreact/src/Stories.js
+++ projeto7-instagramreact/src/Stories.js
@@ -5,9 +5,8 @@ function StoryUser(props){
     return (
         <div class="story">
         <div class="imagem">
-          <img src={props.urlImagem} />
-        </div>
-                
+        <img src={props.urlImagem} />
+      </div>                   
     <div class="usuario"> {props.title} </div>    
     </div>
     )
@@ -15,26 +14,38 @@ function StoryUser(props){
 
 
 function Stories(){
-    const urlImagens = [, 
-    , 
-    "assets/img/barked.svg", 
-    "assets/img/nathanwpylestrangeplanet.svg", 
-    "assets/img/wawawicomics.svg", 
-    "assets/img/respondeai.svg",
-    "assets/img/filomoderna.svg",
-    "assets/img/memeriagourmet.svg"        
+    const story = [
+    <StoryUser urlImagem="assets/img/9gag.svg" title="9gag"/>,
+    <StoryUser urlImagem="assets/img/meowed.svg" title="meowed"/>,
+    <StoryUser urlImagem="assets/img/barked.svg" title="barked"/>,
+    <StoryUser urlImagem="assets/img/nathanwpylestrangeplanet.svg" title="nathanwpylestrangeplanet"/>,
+    <StoryUser urlImagem="assets/img/wawawicomics.svg" title="wawawicomics"/>,
+    <StoryUser urlImagem="assets/img/respondeai.svg" title="respondeai"/>,
+    <StoryUser urlImagem="assets/img/filomoderna.svg" title="filomoderna"/>,
+    <StoryUser urlImagem="assets/img/memeriagourmet.svg" title="memeriagourmet"/>          
     ]
+
+    const newItems = [
+    {urlImagem: 'assets/img/9gag.svg', title:'9gag'},
+    {urlImagem:'assets/img/meowed.svg', title:'meowed'},
+    {urlImagem:'assets/img/barked.svg', title:'barked'},
+    {urlImagem:'assets/img/nathanwpylestrangeplanet.svg', title:'nathanwpylestrangeplanet'},
+    {urlImagem:'assets/img/wawawicomics.svg', title:'wawawicomics'},
+    {urlImagem:'assets/img/respondeai.svg', title:'respondeai'},
+    {urlImagem:'assets/img/filomoderna.svg', title:'filomoderna'},
+    {urlImagem:'assets/img/memeriagourmet.svg', title:'memeriagourmet'} 
+    
+    ]
+    
+    const arrayStory = newItems.map((item) => {
+        return <StoryUser urlImagem={item.urlImagem} title={item.title}/>;
+    })
+
     return (
+
         <div class="stories">
-               
-         <StoryUser urlImagem="assets/img/9gag.svg" title="9gag"/>
-         <StoryUser urlImagem="assets/img/meowed.svg" title="meowed"/>
-         <StoryUser urlImagem="assets/img/barked.svg" title="barked"/>
-         <StoryUser urlImagem="assets/img/nathanwpylestrangeplanet.svg" title="nathanwpylestrangeplanet"/>
-         <StoryUser urlImagem="assets/img/wawawicomics.svg" title="wawawicomics"/>
-         <StoryUser urlImagem="assets/img/respondeai.svg" title="respondeai"/>
-         <StoryUser urlImagem="assets/img/filomoderna.svg" title="filomoderna"/>
-         <StoryUser urlImagem="assets/img/memeriagourmet.svg" title="memeriagourmet"/>
+            
+          {arrayStory}
   
           
        <div class="setinha">
