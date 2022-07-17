1f5c34e71d28811cc45f1eb31a7a798d5f38de72
Ana Carvalho <anapaulacmoura@gmail.com>
2022-07-17 17:22:29 -0300

    criando componentes base

--- projeto7-instagramreact/src/index.js
+++ projeto7-instagramreact/src/index.js
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom';
 import Navbar from './Navbar';
+import Corpo from './Corpo';
+import FundoMobile from '../Fundo-mobile';
 
 function App(){
     return (
         <div>
             <Navbar />
+            <Corpo />
+            <FundoMobile />
         </div>
     );
 }
